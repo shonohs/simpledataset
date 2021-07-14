acdcbdce187c1ecad9fccd8509385dea5ac2a5d4
shono <>
2021-07-14 16:31:33 -0700

    Release 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 
 setuptools.setup(name='simpledataset',
-                 version='0.1.2',
+                 version='0.1.3',
                  description="Utility tools for SIMPLE vision dataset format.",
                  packages=setuptools.find_packages(),
                  install_requires=['pillow', 'requests', 'tenacity', 'tqdm'],
