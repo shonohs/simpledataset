1da8215ad4b074a8e270349a57d1f378e2ee7ced
shono <>
2021-12-21 00:00:44 -0800

    dataset_summary: get the max number of boxes per image.

--- simpledataset/commands/summary.py
+++ simpledataset/commands/summary.py
@@ -7,13 +7,17 @@ from simpledataset.common import SimpleDatasetFactory
 def print_summary(main_txt_filepath):
     dataset = SimpleDatasetFactory().load(main_txt_filepath)
     num_class_samples = collections.Counter()
+    additional_summaries = []
 
     if dataset.type == 'image_classification':
         for image, labels in dataset:
             num_class_samples.update(labels)
     elif dataset.type == 'object_detection':
+        max_num_boxes_per_image = 0
         for image, labels in dataset:
             num_class_samples.update(x[0] for x in labels)
+            max_num_boxes_per_image = max(max_num_boxes_per_image, len(labels))
+        additional_summaries.append(f"The max number of boxes per image: {max_num_boxes_per_image}")
     elif dataset.type == 'visual_relationship':
         for image, labels in dataset:
             num_class_samples.update(x[0] for x in labels)
@@ -26,6 +30,10 @@ def print_summary(main_txt_filepath):
     print(f"The number of images: {len(dataset)}")
     print(f"The number of classes: {len(num_class_samples)}")
     print(f"Max class id: {dataset.get_max_class_id()}")
+
+    for s in additional_summaries:
+        print(s)
+
     print("Class distribution:")
     labels = dataset.labels
     for class_id in sorted(num_class_samples.keys()):
